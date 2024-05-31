d5911485688dbb5bd41c5f9dc0a01f4e97928a5f
hywax <me@hywax.space>
2024-05-31 23:53:56 +0500

    chore(web): connect ui kit

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -1,6 +1,41 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  srcDir: 'src',
+  modules: ['@nuxt/fonts', 'nuxt-primevue'],
+  primevue: {
+    importTheme: {
+      from: '~~/theme.config',
+    },
+  },
+  fonts: {
+    provider: 'google',
+    families: [
+      {
+        name: 'Inter',
+        provider: 'google',
+        weights: [300, 400, 500, 600, 700, 800, 900],
+      },
+    ],
+    defaults: {
+      fallbacks: {
+        serif: [
+          '-apple-system',
+          'BlinkMacSystemFont',
+          'Segoe UI',
+          'Roboto',
+          'Helvetica',
+          'Arial',
+          'sans-serif',
+          'Apple Color Emoji',
+          'Segoe UI Emoji',
+          'Segoe UI Symbol',
+        ],
+      },
+    },
+  },
+  css: [
+    '~/assets/css/main.css',
+    'primeicons/primeicons.css',
+  ],
   nitro: {
     esbuild: {
       options: {
