a20f3be5dc3f17be07d62220e5c7a57704ea23d7
hywax <me@hywax.space>
2024-06-02 02:29:51 +0500

    chore(web): migrate to nuxt ui

--- apps/web/nuxt.config.ts
+++ apps/web/nuxt.config.ts
@@ -4,19 +4,14 @@ export default defineNuxtConfig({
   },
   srcDir: 'src',
   modules: [
+    '@nuxt/ui',
     '@nuxt/fonts',
     '@nuxtjs/color-mode',
-    '@nuxtjs/tailwindcss',
-    'nuxt-primevue',
   ],
-  primevue: {
-    usePrimeVue: true,
-    importTheme: {
-      from: '~~/theme.config',
-    },
-  },
   colorMode: {
     classSuffix: '',
+    preference: 'dark',
+    fallback: 'light',
   },
   fonts: {
     provider: 'google',
@@ -27,33 +22,8 @@ export default defineNuxtConfig({
         weights: [300, 400, 500, 600, 700, 800, 900],
       },
     ],
-    defaults: {
-      fallbacks: {
-        serif: [
-          '-apple-system',
-          'BlinkMacSystemFont',
-          'Segoe UI',
-          'Roboto',
-          'Helvetica',
-          'Arial',
-          'sans-serif',
-          'Apple Color Emoji',
-          'Segoe UI Emoji',
-          'Segoe UI Symbol',
-        ],
-      },
-    },
   },
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
-  },
-  css: [
-    'primeicons/primeicons.css',
-    '~/assets/css/main.css',
-  ],
+  css: ['~/assets/css/main.css'],
   nitro: {
     esbuild: {
       options: {
--- apps/web/tailwind.config.ts
+++ apps/web/tailwind.config.ts
@@ -1,20 +1,41 @@
 import type { Config } from 'tailwindcss'
-// @ts-expect-error no declared types at this time
-import primeui from 'tailwindcss-primeui'
+import defaultTheme from 'tailwindcss/defaultTheme'
 
 export default <Partial<Config>>{
   darkMode: 'class',
-  corePlugins: {
-    preflight: false,
-  },
-  plugins: [primeui],
   theme: {
-    screens: {
-      'sm': '576px',
-      'md': '768px',
-      'lg': '992px',
-      'xl': '1200px',
-      '2xl': '1920px',
+    extend: {
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        'electric-violet': {
+          50: '#f4f2ff',
+          100: '#eae8ff',
+          200: '#d7d4ff',
+          300: '#bab1ff',
+          400: '#9785ff',
+          500: '#6c47ff',
+          600: '#6430f7',
+          700: '#561ee3',
+          800: '#4818bf',
+          900: '#3c169c',
+          950: '#230b6a',
+        },
+        'mantis': {
+          50: '#f6faf3',
+          100: '#e9f5e3',
+          200: '#d3eac8',
+          300: '#afd89d',
+          400: '#82bd69',
+          500: '#61a146',
+          600: '#4c8435',
+          700: '#3d692c',
+          800: '#345427',
+          900: '#2b4522',
+          950: '#13250e',
+        },
+      },
     },
   },
 }
