3e124791d02ae75a2ca897eb9529189c7bb70b53
hywax <me@hywax.space>
2024-06-03 20:27:14 +0500

    chore(web): head i18n

--- apps/web/src/app.vue
+++ apps/web/src/app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const i18nHead = useLocaleHead({
+  addSeoAttributes: {},
+})
+
+useHead({
+  htmlAttrs: {
+    lang: i18nHead.value.htmlAttrs!.lang,
+  },
+  link: [...(i18nHead.value.link || [])],
+  meta: [...(i18nHead.value.meta || [])],
+})
+</script>
