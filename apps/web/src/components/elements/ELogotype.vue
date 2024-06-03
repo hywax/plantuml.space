602f51e6c4f4b8cfd3e1c8618e7311baaf131ed5
hywax <me@hywax.space>
2024-06-03 20:30:43 +0500

    chore(web): logotype component

---
+++ apps/web/src/components/elements/ELogotype.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="flex items-center gap-1.5">
+    <NuxtLink to="/" class="flex-shrink-0 font-bold text-xl text-gray-900 dark:text-white flex items-center">
+      Plant<span class="text-primary">UML</span>
+    </NuxtLink>
+  </div>
+</template>
