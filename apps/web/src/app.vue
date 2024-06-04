f73a1c88a66fad8a88d0011beae5c7e67a9ca210
hywax <me@hywax.space>
2024-06-04 20:08:32 +0500

    chore(web): add dir attribute in head

--- apps/web/src/app.vue
+++ apps/web/src/app.vue
@@ -7,11 +7,14 @@
 <script setup lang="ts">
 const i18nHead = useLocaleHead({
   addSeoAttributes: {},
+  addDirAttribute: true,
+  identifierAttribute: 'id',
 })
 
 useHead({
   htmlAttrs: {
-    lang: i18nHead.value.htmlAttrs!.lang,
+    lang: i18nHead.value.htmlAttrs?.lang,
+    dir: i18nHead.value.htmlAttrs?.dir,
   },
   link: [...(i18nHead.value.link || [])],
   meta: [...(i18nHead.value.meta || [])],
